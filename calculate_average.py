719b7fc3cc3835e4ad502e32c04d26783830f948
Haizy Nguyen <hainguyensgu22@gmail.com>
2026-02-02 16:49:49 +0700

    Add calculate_Average.py

--- calculate_average.py
+++ calculate_average.py
@@ -1,4 +1,4 @@
-print(f"Nhập 3 so tu nguoi dung ")
+print(f"Nhập 3 số từ người dùng ")
 number1 = input("Nhập số thứ nhất: ")
 number2 = input("Nhập số thứ hai: ")
 number3 = input("Nhập số thứ ba: ")
