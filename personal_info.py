ca5c5965230df6b5fa81c1aae70adc158c2c01db
Haizy Nguyen <hainguyensgu22@gmail.com>
2026-02-02 15:18:27 +0700

    Add Python scripts: personal_info.py and Lesson1.py

---
+++ personal_info.py
@@ -0,0 +1,8 @@
+name = input("Nhập tên của bạn: ")
+age = input("Nhập tuổi của bạn: ")
+address = input("Nhập địa chỉ của bạn: ")
+
+print("\n-- Thông tin cá nhân ---")
+print(f"Họ và tên: {name}")
+print(f"Tuổi: {age}")   
+print(f"Địa chỉ: {address}")
