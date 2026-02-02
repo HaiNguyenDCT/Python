001e784dc5fe46f693af8c61260c8d1ad5992725
Haizy Nguyen <hainguyensgu22@gmail.com>
2026-02-02 16:47:50 +0700

    Add calculate_Average.py

---
+++ calculate_average.py
@@ -0,0 +1,7 @@
+print(f"Nhập 3 so tu nguoi dung ")
+number1 = input("Nhập số thứ nhất: ")
+number2 = input("Nhập số thứ hai: ")
+number3 = input("Nhập số thứ ba: ")
+
+ave = (float(number1) + float(number2) + float(number3)) / 3
+print(f"Trung bình cộng của 3 số là: {ave}")        
