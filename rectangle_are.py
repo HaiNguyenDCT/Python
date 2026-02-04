e8152577bcb019575448c5bc11092e854d111462
Haizy Nguyen <hainguyensgu22@gmail.com>
2026-02-04 14:47:19 +0700

    Add rectangle_are.py

---
+++ rectangle_are.py
@@ -0,0 +1,6 @@
+length = float(input("Nhập chiều dài hình chữ nhật: "))
+width = float(input("Nhập chiều rộng hình chữ nhật: "))
+
+area = length * width
+
+print(f"Diện tích hình chữ nhật là: {area}")
